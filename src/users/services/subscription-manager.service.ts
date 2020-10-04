717365f9df6129cd535d5153c933157e856c1265
Marais <piemarais@gmail.com>
2020-10-04 21:17:25 +0200

    Init Sendinblue

--- src/users/services/subscription-manager.service.ts
+++ src/users/services/subscription-manager.service.ts
@@ -4,6 +4,7 @@ import { ValidationError } from 'apollo-server-express';
 import { Repository } from 'typeorm';
 import { SubscriptionInput } from '../inputs/subscription.input';
 import { User } from '../models/user.model';
+import * as SibApiV3Sdk from "sib-api-v3-sdk";
 
 @Injectable()
 export class SubscriptionManager {
@@ -19,14 +20,37 @@ export class SubscriptionManager {
 
     const user = await User.subscribe(subscription);
 
+    this.sendConfirmationEmail();
+
     return this.usersRepository.save(user);
   }
 
-  private async isEmailAlreadyUsed(email: string) {
+  private async isEmailAlreadyUsed(email: string): Promise<boolean> {
     const users = await this.usersRepository.find({
       where: { email: email },
     });
 
     return users.length !== 0;
+  }
+
+  private async sendConfirmationEmail() : Promise<void> {
+    const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+    const apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = "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"
+
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    const templateId = 1;
+
+    const sendEmail = new SibApiV3Sdk.SendEmail();// SendEmail |
+
+    sendEmail.emailTo = ['piemarais@gmail.com'];
+
+    apiInstance.sendTemplate(templateId, sendEmail).then(function(data) {
+      console.log('API called successfully. Returned data: ' + data);
+    }, function(error) {
+      console.error(error);
+    });
   }
 }
