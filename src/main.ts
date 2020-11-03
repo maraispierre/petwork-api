053d8a3e74ffd033415667bf801e81cd3cc10e6c
Marais <piemarais@gmail.com>
2020-11-03 18:37:15 +0100

    Format code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const httpsKeyPath : string = process.env.HTTPS_KEY_PATH ?? '';
-const httpsCertPath : string = process.env.HTTPS_CERT_PATH ?? '';
+const httpsKeyPath: string = process.env.HTTPS_KEY_PATH ?? '';
+const httpsCertPath: string = process.env.HTTPS_CERT_PATH ?? '';
 
 console.log('test' + httpsCertPath);
 
@@ -17,7 +17,7 @@ const httpsOptions = {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    httpsOptions
+    httpsOptions,
   });
   await app.listen(3000);
 }
