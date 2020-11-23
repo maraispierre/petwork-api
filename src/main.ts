3255748df9e90fbac34ad9f3018db5abc76d4164
Marais <piemarais@gmail.com>
2020-11-23 18:56:45 +0100

    Remove https management from nest application

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-const httpsKeyPath: string = process.env.HTTPS_KEY_PATH ?? '';
-const httpsCertPath: string = process.env.HTTPS_CERT_PATH ?? '';
-
-const httpsOptions = {
-  key: fs.readFileSync(httpsKeyPath),
-  cert: fs.readFileSync(httpsCertPath),
-};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(80);
 }
 bootstrap();
